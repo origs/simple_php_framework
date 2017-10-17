62d13d68b0765557c70a3048d026d3e74ec1c76f
VictorFursa <victor.fursa.ck@gmail.com>
2017-10-18 00:34:45 +0300

    create template, and create simple application 'is a leap year or not'

--- web/index.php
+++ web/index.php
@@ -6,6 +6,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
+function render_template($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    /** sprintf return formatted string */
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
+
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/route.php';
 
@@ -14,12 +24,8 @@ $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    /** sprintf return formatted string */
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-
-    $response = new Response(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func($request->attributes->get('_controller'), $request);
 } catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
 } catch (Exception $e) {
